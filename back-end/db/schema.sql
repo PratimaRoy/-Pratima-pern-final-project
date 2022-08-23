27e3da47ed494f3fa86b4222aa3ccecfa973c047
PratimaRoy <pratimaroy170@gmail.com>
2022-08-23 12:30:33 -0400

    Created schema table and added data to the seed

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -3,9 +3,31 @@ CREATE DATABASE cta_dev;
 
 \c cta_dev;
 
-DROP TABLE IF EXISTS test;
+DROP TABLE IF EXISTS products;
 
-CREATE TABLE test (
+CREATE TABLE products (
     id SERIAL PRIMARY KEY, 
-    name TEXT
+    name TEXT,
+    image TEXT,
+    description TEXT,
+    color TEXT,
+    price INTEGER,
+    rating INTEGER,
+    CHECK (rating >= 0 AND rating <= 5),
+    featured BOOLEAN
+    
 );
+
+DROP TABLE IF EXISTS reviews;
+
+CREATE TABLE reviews (
+    id SERIAL PRIMARY KEY,
+    reviewer TEXT,
+    title TEXT,
+    content TEXT,
+    rating NUMERIC,
+    CHECK (rating >= 0 AND rating <= 5),
+    products_id INTEGER REFERENCES products (id)
+    ON DELETE CASCADE
+);
+
--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -1,10 +1,18 @@
 \c cta_dev;
 
-INSERT INTO test (name) VALUES
-('Monday'),
-('Tuesday'),
-('Wednesday'),
-('Thursday'),
-('Friday'),
-('Saturday'),
-('Sunday');
+INSERT INTO products (name, description,color, price, rating, featured, image) VALUES
+('Gold Mayur Mugh Bala','Gold Peacock Bengali Bala Bangle', 'Gold', 4000, 5, true, 'https://static.malabargoldanddiamonds.com/media/catalog/product/cache/5/image/9df78eab33525d08d6e5fb8d27136e95/b/c/bcctbib01253.jpg' ),
+('Tamil Bride Divine Necklace', 'Gold Tamil Necklace for Wedding', 'Gold', 5000, 5, true, 'https://static.malabargoldanddiamonds.com/media/catalog/product/cache/5/image/9df78eab33525d08d6e5fb8d27136e95/n/e/nedicdtrsda418.jpg'),
+('Precia Gemstone Earring', 'Ruby with gold earrings for daily wear or any occasion', 'Red', 700, 4, true, 'https://static.malabargoldanddiamonds.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/s/t/stprhdflhia314_n.jpg'),
+('Teardrop Halo and Wishbone Stacking Ring Set', 'A 2 diamond ring set for engagement', 'White', 160, 5, true, 'https://cdn-fsly.yottaa.net/5c17f01f2bb0ac532957f9ee/us.pandora.net/v~4b.1d/dw/image/v2/AAVX_PRD/on/demandware.static/-/Sites-pandora-master-catalog/default/dw0c732706/productimages/NAMPS0179.jpg?sw=900&sh=900&sm=fit&sfrm=png&bgcolor=F5F5F5&yocs=A_F_' ),
+('Gold Plated Multicolor Tikka', 'Indian jewelry worn on the head for any special occasions or temple', 'Rainbow', 50, 5, true, 'https://cdn11.bigcommerce.com/s-ls8hir/images/stencil/original/products/17474/21341/CZTIKKA1__68822.1604600800.jpg?c=2&imbypass=on&imbypass=on');
+
+
+
+INSERT INTO reviews (products_id, reviewer, title, content, rating) VALUES
+
+(1, 'Lisa', 'Gorgeous Bala Bangle', 'Love to wear it when going to a party!', 5 ),
+(2, 'Jennie', 'Wondeful Bride Necklace', 'Loved wearing it to the wedding!', 5 ),
+(3, 'Puja', 'My favorite earrings to wear in the temple!', 'It is not heavy and simple', 5 ),
+(4, 'Julia', 'Gorgeous ring!', 'I loved the teardrop design!', 5 ),
+(5, 'Julie', 'Exquisite Tikka', 'Love to wear it when going to a party, wedding, or temple!', 5 );
